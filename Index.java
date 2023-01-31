85a2fabdd3e9363f953f2c780f1a474d38861239
ashirN1501 <nadarashir1501@gmail.com>
2023-01-31 16:43:05 +0530

    created pattern logic in index.java

--- Index.java
+++ Index.java
@@ -1,5 +1,11 @@
 public class Index{
     public static void main(String args[]){
-        
+        int row = 7, i, j;
+        for(i=1; i<=row; i++){
+            for(j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
